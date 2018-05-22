7bf5ade64aed0fca6022a29077a9cfd4c76ae3fc
Adam Tankanow <adam.tankanow@gmail.com>
2018-05-22 13:12:49 -0400

    CORE-535: move other smoke tests to sam

--- lambda-dynamo/smoke.sh
+++ lambda-dynamo/smoke.sh
@@ -1,25 +1,15 @@
 #!/usr/bin/env bash
+set -e
 
-declare -i i=0
+namespace=${1} ; shift
 
-while true ; do \
-  dt=`date "+%Y-%m-%d %H:%M:%S"`
-  put_request=$(aws lambda invoke \
-                    --function-name "arn:aws:lambda:us-east-1:975482786146:function:lambda-dynamo-live-PutFunction-GEXEQ77I6MGF" \
-                    --invocation-type RequestResponse \
-                    --payload "{\"body\": {\"id\": \"${i}\", \"data\": \"foo\"}}" \
-                    put.out | jq -e '.StatusCode')
-	printf "${dt}: Putting ${i}: ${put_request}\n"
-  get_request=$(aws lambda invoke \
-                    --function-name "arn:aws:lambda:us-east-1:975482786146:function:lambda-dynamo-live-GetFunction-1A8EMVK06ANYE" \
-                    --invocation-type RequestResponse \
-                    --payload "{\"body\": {\"id\": \"${i}\", \"data\": \"foo\"}}" \
-                    get.out | jq -e '.StatusCode')
-	printf "${dt}: Getting ${i}: ${get_request}\n"
-	for i in {1..30} ; do
-	  printf "."
-	  sleep 1
-	done
-	echo
-  i=$(( i + 1 ))
-done
+: ${namespace?}
+
+TABLE_NAME=$(make namespace=live action=describe lambda-dynamo |
+                        jq -re '.Stacks[0].Outputs | map(select(.Description == "Table Arn")) | .[0].OutputValue' |
+                        cut -f2 -d'/')
+
+: ${TABLE_NAME?}
+
+echo '{"body": {"id": "1", "data": "sam local"}}' | TABLE_NAME=${TABLE_NAME} sam local invoke -t lambda-dynamo/template.yaml PutFunction
+echo '{"body": {"id": "1", "data": "sam local"}}' | TABLE_NAME=${TABLE_NAME} sam local invoke -t lambda-dynamo/template.yaml GetFunction
