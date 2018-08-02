7c275a06e602c8f72954dae152a5baa0ef79ca6a
Adam Tankanow <adam.tankanow@gmail.com>
2018-08-02 13:10:11 -0400

    fixes #19: APIG -> Lambda w/ XRay

---
+++ apig-lambda-xray/smoke.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+directory=${0%/*}
+system=${directory#./}
+
+namespace=${1} ; shift
+
+: ${namespace?}
+: ${system?}
+
+url=$(make namespace=${namespace} action=describe ${system} |
+                 jq -re '.Stacks[0].Outputs | map(select(.Description == "API endpoint URL")) | .[0].OutputValue')
+
+: ${url?}
+
+declare i=0
+while (( $i <= 10 )) ; do
+    http GET "${url}"
+    i=$(( i + 1 ))
+done
