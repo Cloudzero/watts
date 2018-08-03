715163e6fa504c4e4d37620e02d3f6e2f766ba86
Adam Tankanow <adam.tankanow@gmail.com>
2018-08-03 06:48:53 -0400

    sns-lambda smoke test invokes deployed components

--- sns-lambda/smoke.sh
+++ sns-lambda/smoke.sh
@@ -1,4 +1,28 @@
 #!/usr/bin/env bash
-
 set -e
-sam local generate-event sns | sam local invoke -t sns-lambda/template.yaml SnsSubscribedFunction
+set -x
+
+directory=${0%/*}
+system=${directory#./}
+
+namespace=${1} ; shift
+
+: ${namespace?}
+: ${system?}
+
+# sam local generate-event sns | sam local invoke -t ${system}/template.yaml SnsSubscribedFunction
+
+topic_arn=$(make namespace=${namespace} action=describe ${system} |
+              jq -re '.Stacks[0].Outputs | map(select(.Description == "SNS Topic Arn")) | .[0].OutputValue' )
+
+: ${topic_arn?}
+
+declare -i i=0
+declare -ir N=10
+while (( ${i} < ${N} )) ; do
+    id=`date +%s`.${i}
+    aws sns publish \
+        --topic-arn "${topic_arn}" \
+        --message "${id}"
+    i=$(( i + 1 ))
+done
