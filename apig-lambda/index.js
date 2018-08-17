49c35e7139baba27213e9bae7b55fcf5384916e0
Adam Tankanow <adam.tankanow@gmail.com>
2018-08-17 13:44:42 -0400

    fixes #24: Name all js files "index.js"

--- apig-lambda/index.js
+++ apig-lambda/index.js
@@ -1,3 +1,6 @@
+// Copyright (c) 2016-present, CloudZero, Inc. All rights reserved.
+// Licensed under the BSD-style license. See LICENSE file in the project root for full license information.
+
 'use strict';
 
 const createResponse = (statusCode, body) => {
